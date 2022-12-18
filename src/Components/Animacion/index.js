4988f5d64f0e30c4b8c9c0057d19330368aafa40
unknown <leandro.florentin@hotmail.com>
2022-12-18 17:51:15 -0300

    sassCommit

---
+++ src/Components/Animacion/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Animacion extends React.Component {
+    render() {
+        return (
+            <>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                    <div className='animacion'>
+                    </div>
+                </div>
+                <div className='containerLineas'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Animacion;
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './scss/main.scss'
+import Animacion from './Components/Animacion';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Animacion />
+        )
+    }
+}
+
+export default App;
